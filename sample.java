4bed3a8883e7f8df61841a91ed0374566da7359b
rvnaathan1407 <57092614+rvnaathan1407@users.noreply.github.com>
2019-11-25 21:34:32 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,9 @@
+public int getScore() {
+    int result;
+    result = (int)(Math.random() * 6) + 1;
+    dice[0].setFaceValue(result);
+    result = (int)(Math.random() * 6) + 1;
+    dice[1].setFaceValue(result);
+    int score = dice[0].getFaceValue() + dice[1].getFaceValue();
+    return score;
+}
